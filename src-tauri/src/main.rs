8a09d115812ef6f5b83e69f0716b11a145de3e90
Danil Karpenko <limpix31@gmail.com>
2023-11-16 14:51:25 +0100

    feat: apply blur on other platforms

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,35 @@
 
 mod commands;
 
+use tauri::Manager;
+#[cfg(target_os = "windows")]
+use window_vibrancy::apply_blur;
+
+#[cfg(target_os = "macos")]
+use window_vibrancy::{
+  apply_vibrancy,
+  NSVisualEffectMaterial,
+};
+
 fn main() {
-	tauri::Builder::default()
-		.invoke_handler(tauri::generate_handler![commands::providers::mojang_list_versions])
-		.run(tauri::generate_context!())
-		.expect("error while running tauri application");
+  tauri::Builder::default()
+    .setup(|app| {
+			#[allow(unused)]
+      let window = app.get_window("main").unwrap();
+
+      #[cfg(target_os = "macos")]
+      apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, None)
+        .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+      #[cfg(target_os = "windows")]
+      apply_blur(&window, None)
+        .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
+      Ok(())
+    })
+    .invoke_handler(tauri::generate_handler![
+      commands::providers::mojang_list_versions
+    ])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
