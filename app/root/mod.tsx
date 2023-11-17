3334ba43ed4a6f4e3fb245ab982c9f7ca353e04d
Danil Karpenko <limpix31@gmail.com>
2023-11-17 10:00:38 +0100

    chore: github workflows

--- app/root/mod.tsx
+++ app/root/mod.tsx
@@ -1,11 +1,3 @@
-import { Column } from '@ui/layout'
-import { Row }    from '@ui/layout'
-
 export function Root() {
-	return (
-		<Column p={4} gap={5}>
-			<Row>Row1</Row>
-			<Row>Row 1</Row>
-		</Column>
-	)
+	return 'Hello!'
 }
