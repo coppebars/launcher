30dad28b75a76fd571ca279ae31ef27f7823ae93
Danil Karpenko <limpix31@gmail.com>
2023-11-19 10:42:36 +0100

    feat: test command invoke

--- app/root/mod.tsx
+++ app/root/mod.tsx
@@ -1,3 +1,13 @@
+import { useEffect } from 'react'
+
+import { invoke }    from '@tauri-apps/api/primitives'
+
 export function Root() {
+	useEffect(() => {
+		void (async () => {
+			console.log(await invoke('lookup_versions', { path: '/home/limpix/workspaces/launcher/minecraft' }))
+		})()
+	})
+
 	return 'Hello!'
 }
